9219bb59c5967a91134caf17ea9f8649e8c22f89
Roman Pylenkov <solodov1994roma@gmail.com>
2020-09-27 21:44:57 +0300

    Added CountriesHandler class as adapter between files and logic

---
+++ myapp/src/main/java/ru/pr/lab00/myapp/CountriesHandler.java
@@ -0,0 +1,24 @@
+package ru.pr.lab01.myapp;
+
+import java.util.Locale;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
+
+public class CountriesHandler {
+	private static final String BUNDLE_BASE_NAME = "capitals";
+	private String language = "en";
+
+	public void setLanguage(String language) { this.language = language; }
+
+	public Country getCountry(String countryName) {
+		ResourceBundle capitals = getCapitals(this.language);
+
+	    String capital = capitals.getString(countryName);
+        
+        return new Country(countryName, capital);
+	}
+
+    private ResourceBundle getCapitals(String language) {
+        return PropertyResourceBundle.getBundle(BUNDLE_BASE_NAME, new Locale(language));
+    }
+}
