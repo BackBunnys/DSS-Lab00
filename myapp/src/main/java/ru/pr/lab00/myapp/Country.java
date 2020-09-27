cab88d0e375f48c5130986a7812b29a7fbe7b467
Roman Pylenkov <solodov1994roma@gmail.com>
2020-09-27 21:38:32 +0300

    Added Country class

---
+++ myapp/src/main/java/ru/pr/lab00/myapp/Country.java
@@ -0,0 +1,22 @@
+package ru.pr.lab01.myapp;
+
+public class Country {
+	private String name; //Страна имеет название
+	private String capital; //И свою столицу
+
+	public Country(String name, String capital) {
+		setName(name);
+		setCapital(capital);
+	}
+
+	public void setName(String name) { this.name = name; }
+	public String getName() { return this.name; }
+
+	public void setCapital(String capital) { this.capital = capital; }
+	public String getCapital() { return this.capital; }
+
+	@Override
+	public String toString() {
+		return name + " - " + capital;
+	}
+}
