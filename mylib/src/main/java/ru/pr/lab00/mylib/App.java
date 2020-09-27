e68e4afb02df70dc6455f6241108eea6919ca839
Roman Pylenkov <solodov1994roma@gmail.com>
2020-09-27 23:00:49 +0300

    Generated second application

---
+++ mylib/src/main/java/ru/pr/lab00/mylib/App.java
@@ -0,0 +1,13 @@
+package ru.pr.lab00.mylib;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
